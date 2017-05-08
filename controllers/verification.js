50328e7b2e33d4849edd66e4f1ec2493c7840867
diewoo <dgo250594@gmail.com>
2017-05-08 11:34:47 -0500

    5%

--- controllers/verification.js
+++ controllers/verification.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
     const hubChallenge = req.query['hub.challenge'];
 
     const hubMode = req.query['hub.mode'];
-    const verifyTokenMatches = (req.query['hub.verify_token'] === 'packbot is cool');
+    const verifyTokenMatches = (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN);
 
     if (hubMode && verifyTokenMatches) {
         res.status(200).send(hubChallenge);
